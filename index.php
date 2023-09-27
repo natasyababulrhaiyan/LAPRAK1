0e735fb8d4e2a402a3382a4202279cec774d8384
natasyababulrhaiyan <natasyalsm@gmail.com>
2023-09-27 10:15:47 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+    echo "ini adalah script PHP pertama saya <br><br>";
+
+    $angka1 = 3224;
+    $angka2 = 7787;
+
+    $jumlah = $angka1 * $angka2;
+    echo "jumlah dari ". $angka1. "x". $angka2. " = <b>".$jumlah. "<b>"
+?>
